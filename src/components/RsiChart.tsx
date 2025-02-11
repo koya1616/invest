22a865dfc8055848a86b368b312a9125dee8aee3
koya1616 <koya1616.0622@gmail.com>
2025-02-11 14:42:18 +0800

    Add DataRangeSlider to RsiChart for adjustable data length

--- src/components/RsiChart.tsx
+++ src/components/RsiChart.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { ComposedChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Line, ResponsiveContainer } from "recharts";
+import DataRangeSlider from "./DataRangeSlider";
 
 export const RsiChart = ({
   data,
@@ -10,10 +12,12 @@ export const RsiChart = ({
     rsi: number | null;
   }[];
 }) => {
+  const [dataLength, setDataLength] = useState(data.length);
+  const dataSlice = data.slice(data.length - dataLength);
   return (
-    <div className="w-full h-40">
-      <ResponsiveContainer width="100%" height="100%">
-        <ComposedChart data={data} margin={{ top: 0, right: 20, bottom: 0, left: 0 }} barGap={-3}>
+    <div className="w-full h-60">
+      <ResponsiveContainer width="100%" height="90%">
+        <ComposedChart data={dataSlice} margin={{ top: 0, right: 20, bottom: 0, left: 0 }} barGap={-3}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
@@ -22,6 +26,8 @@ export const RsiChart = ({
           <Line dot={false} dataKey="rsi" stroke="blue" />
         </ComposedChart>
       </ResponsiveContainer>
+
+      <DataRangeSlider value={dataLength} max={data.length} onChange={setDataLength} />
     </div>
   );
 };
