a8267459712560ecb911facb4e721b60210765f5
koya1616 <koya1616.0622@gmail.com>
2025-02-11 10:24:18 +0800

    Refactor date formatting functions to support additional formats

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,9 +1,12 @@
-export const formatTimestamp = (timestamp: number, format: "time" | "day") => {
+export const formatTimestamp = (timestamp: number, format: string) => {
   const date = new Date(timestamp * 1000 + (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000);
-  if (format === "time") {
+  if (format === "1m" || format === "5m") {
     return formatTime(date);
   }
-  return formatDate(date);
+  if (format === "1d") {
+    return formatDate(date);
+  }
+  return formatDateTime(date);
 };
 
 const formatTime = (date: Date) => {
@@ -15,8 +18,13 @@ const formatTime = (date: Date) => {
 const formatDate = (date: Date) => {
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
+  return `${month}/${day}`;
+};
+
+const formatDateTime = (date: Date) => {
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-
   return `${month}/${day} ${hours}:${minutes}`;
 };
