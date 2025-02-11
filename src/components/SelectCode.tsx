696e176e7def944bd95835648848682b6f5ab341
koya1616 <koya1616.0622@gmail.com>
2025-02-11 11:56:10 +0800

    Add Toyota option to SelectCode component for stock selection

--- src/components/SelectCode.tsx
+++ src/components/SelectCode.tsx
@@ -12,6 +12,7 @@ export const SelectCode = ({ code }: { code: string }) => {
       className="p-2 mb-4 border border-gray-300 rounded-md"
     >
       <option value="">選択する</option>
+      <option value="7203">7203 トヨタ</option>
       <option value="9432">9432 NTT</option>
       <option value="9433">9433 KDDI</option>
     </select>
