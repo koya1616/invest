622c7e982bb3a037b662bafc9f5887b40a8bab83
koya1616 <koya1616.0622@gmail.com>
2025-02-12 13:47:39 +0800

    Add SelectInterval component to Page for interval selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import SelectCode from "@/components/SelectCode";
+import SelectInterval from "@/components/SelectInterval";
 
 export default function Page() {
-  return <SelectCode code="" />;
+  return (
+    <>
+      <p>チャート画面</p>
+      <SelectCode code="" />
+      <p>予想画面</p>
+      <SelectInterval interval="" />
+    </>
+  );
 }
