3798194891f5a2b3648521d1819e69bfc3095b0f
koya1616 <koya1616.0622@gmail.com>
2025-02-12 16:33:29 +0800

    Add dynamic loading of Prediction components for multiple stock codes

--- src/app/prediction/[interval]/page.tsx
+++ src/app/prediction/[interval]/page.tsx
@@ -1,6 +1,8 @@
+import Prediction from "@/components/Prediction";
 import ReloadButton from "@/components/ReloadButton";
 import SelectInterval from "@/components/SelectInterval";
 import ToHomeButton from "@/components/ToHomeButton";
+import { Suspense } from "react";
 
 export default async function Page({
   params,
@@ -8,11 +10,18 @@ export default async function Page({
   params: Promise<{ interval: string }>;
 }) {
   const p = await params;
+  const codes = ["7203", "8306", "8591", "9432", "9433"];
   return (
     <>
       <ToHomeButton />
       <SelectInterval interval={p.interval} />
       <ReloadButton />
+
+      {codes.map((code) => (
+        <Suspense key={code} fallback={<div>Loading...</div>}>
+          <Prediction code={code} interval={p.interval} />
+        </Suspense>
+      ))}
     </>
   );
 }
