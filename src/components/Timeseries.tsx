c1c013a5af242a7ab955c5b925bc95c53a7b1f90
koya1616 <koya1616.0622@gmail.com>
2025-02-12 19:39:13 +0800

    Add Timeseries component for displaying stock price data with SMA calculations

---
+++ src/components/Timeseries.tsx
@@ -0,0 +1,39 @@
+import { formatDateTimeString } from "@/lib/date";
+import StockPriceChart from "./StockPriceChart";
+import { calculateSma } from "@/lib/calculate";
+import { fetchTimeseries, type MarketDataResponse } from "@/actions/timeseries";
+
+const Timeseries = async ({ code, interval }: { code: string; interval: string }) => {
+  const data = await fetchTimeseries(code, interval);
+  const formattedPrice = formatPrice(data, interval);
+  return (
+    <>
+      <StockPriceChart data={formattedPrice.data} min={formattedPrice.min} max={formattedPrice.max} />
+    </>
+  );
+};
+
+const formatPrice = (data: MarketDataResponse, interval: string) => {
+  const sma: number[] = [];
+  const formattedData = data.series.map((item) => {
+    const { open, close, high, low } = item;
+
+    sma.push(close);
+
+    return {
+      name: formatDateTimeString(item.dateTime_str, interval),
+      openClose: [open, close],
+      lowHigh: [low, high],
+      sma5: calculateSma(sma, 5),
+      sma10: calculateSma(sma, 10),
+      sma20: calculateSma(sma, 20),
+      sma25: calculateSma(sma, 25),
+      sma75: calculateSma(sma, 75),
+      fill: close < open ? "#ef4444" : "#22c55e",
+    };
+  });
+
+  return { data: formattedData, min: Math.min(...sma), max: Math.max(...sma) };
+};
+
+export default Timeseries;
