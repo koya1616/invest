0284afcfe64287adf87f6874ca45aa1c26688ec7
koya1616 <koya1616.0622@gmail.com>
2025-02-12 14:10:36 +0800

    Add ToHomeButton component for navigation to home page

---
+++ src/components/ToHomeButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const ToHomeButton = () => {
+  const router = useRouter();
+  return (
+    <button
+      type="button"
+      onClick={() => router.push("/")}
+      className="border-2 border-blue-500 text-blue-500 py-2 px-4 rounded"
+    >
+      ホーム
+    </button>
+  );
+};
+
+export default ToHomeButton;
