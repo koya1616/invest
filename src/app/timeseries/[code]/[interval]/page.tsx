13c96875a2b7ea51f19ce17467ae6a38aab50bd7
koya1616 <koya1616.0622@gmail.com>
2025-02-13 21:00:58 +0800

    Add Revenue and Prediction components to timeseries page for enhanced data visualization

--- src/app/timeseries/[code]/[interval]/page.tsx
+++ src/app/timeseries/[code]/[interval]/page.tsx
@@ -1,5 +1,7 @@
 import Loading from "@/components/Loading";
+import Prediction from "@/components/Prediction";
 import ReloadButton from "@/components/ReloadButton";
+import Revenue from "@/components/Revenue";
 import SelectCode from "@/components/SelectCode";
 import SelectInterval from "@/components/SelectInterval";
 import Timeseries from "@/components/Timeseries";
@@ -18,6 +20,14 @@ export default async function Page({
       <SelectCode code={p.code} />
       <SelectInterval code={p.code} interval={p.interval} />
       <ReloadButton />
+      <div className="flex flex-wrap justify-around">
+        <Suspense fallback={<Loading />}>
+          <Revenue code={p.code} name={""} />
+        </Suspense>
+        <Suspense fallback={<Loading />}>
+          <Prediction code={p.code} name={""} interval={p.interval} />
+        </Suspense>
+      </div>
       <Suspense fallback={<Loading />}>
         <Timeseries code={p.code} interval={p.interval} />
       </Suspense>
