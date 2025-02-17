fa941ff8fdc25d3e7e69ed5e0aa2c5c3cb9ea206
koya1616 <koya1616.0622@gmail.com>
2025-02-17 15:05:41 +0800

    Add fetchTimeseriesV2 function to retrieve time series data based on interval

---
+++ src/actions/timeseries-v2.ts
@@ -0,0 +1,19 @@
+interface TimeseriesData {
+  value: number;
+  datetime: string;
+}
+
+export const fetchTimeseriesV2 = async (code: string, interval: string): Promise<TimeseriesData[]> => {
+  const table = interval === "1" ? "one_minute_timeseries" : "five_minutes_timeseries";
+  const response = await fetch(
+    `https://yuzgeodguekogqnxrcdm.supabase.co/rest/v1/${table}?code=eq.${code}&select=*&order=datetime.asc`,
+    {
+      headers: {
+        apikey: `${process.env.SUPABASE_CLIENT_ANON_KEY}`,
+        Authorization: `Bearer ${process.env.SUPABASE_CLIENT_ANON_KEY}`,
+      },
+    },
+  );
+
+  return await response.json();
+};
